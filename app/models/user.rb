75855e087252af777c4bfe8ec3e4d353e9b2accd
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-04-16 19:10:01 -0300

    Add user model associations to activities and groups models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :groups, class_name: 'Group', foreign_key: 'creator_id'
+  has_many :activities, class_name: 'Activity', foreign_key: 'author_id'
+
 
 end
