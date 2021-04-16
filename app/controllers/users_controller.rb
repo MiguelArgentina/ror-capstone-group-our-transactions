5b926eb16de20e79b6941d896892900c98bba9f5
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-04-16 17:11:08 -0300

    Add user model and basic login with username and password

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      flash[:danger] = 'Invalid signup details'
+      redirect_to root_path
+    end
+  end
+
+  def show
+    redirect_to root_path unless session[:user_id]
+    @user = User.find(session[:user_id]) if session[:user_id]
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #
+  resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
+  root 'sessions#login'
+
+
+
+  #routes to handle sessions:
+  get '/login', to: 'sessions#login'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
 end
