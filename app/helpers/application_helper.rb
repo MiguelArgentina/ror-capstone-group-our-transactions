7c4aab7d9e3f587175884cb790620639b9d1b965
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-04-18 22:22:34 -0300

    Add navigation controls for login, logout, sign up, groups

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+
   def logged_in?
     !!session[:user_id]
   end
@@ -6,4 +7,14 @@ module ApplicationHelper
   def current_user
     @current_user ||= User.find_by_id(session[:user_id]) if !!session[:user_id]
   end
+
+  def get_navbar_links
+    if logged_in?
+      render 'layouts/logged_in_user_links'
+    else
+      render 'layouts/logged_out_user_links'
+    end
+  end
+
+
 end
