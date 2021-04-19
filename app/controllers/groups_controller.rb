60433461e7422176c11d47c311957e0f0b26d977
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-04-19 12:19:14 -0300

    Finish CRUD for Groups model

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -23,6 +23,32 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
+  # GET /group/1/edit
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  # PATCH/PUT /products/1
+  def update
+    group = Group.find(params[:id])
+    respond_to do |format|
+      if group.update(group_params)
+        format.html { redirect_to group, notice: 'Product details were successfully updated.' }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /products/1
+  def destroy
+    group = Group.find(params[:id])
+    group.destroy
+    respond_to do |format|
+      format.html { redirect_to groups_url, notice: 'Group was successfully deleted from database.' }
+    end
+  end
+
   private
 
   def group_params
