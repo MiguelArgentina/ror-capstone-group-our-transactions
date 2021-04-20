cd2ff04c1dadae9b6e3a2c6fb7520aaf9635b03d
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-04-20 19:18:42 -0300

    Rename email column to icon

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_16_212422) do
+ActiveRecord::Schema.define(version: 2021_04_20_192535) do
 
   create_table "activities", force: :cascade do |t|
     t.string "name"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2021_04_16_212422) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
-    t.string "email"
+    t.string "icon"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
