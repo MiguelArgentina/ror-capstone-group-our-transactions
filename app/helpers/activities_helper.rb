00b993c1b196b3563a39a030c5491558295a6dc8
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-04-23 15:58:11 -0300

    Move logic out of views

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -6,4 +6,16 @@ module ActivitiesHelper
                              'Activities listing'
                            end
   end
+
+  def icon_url_for_activity_show
+    @groups.nil? ? 'groups/noicon.png' : 'groups/' + @groups.icon
+  end
+  
+  def icon_url_for_activity_index(activity)
+    activity.group.nil? || activity.group.icon.nil? ? 'groups/noicon.png' : 'groups/' + activity.group.icon
+  end
+
+  def group_icon_div_text
+    @groups.nil? ? 'No group' : @groups.name
+  end
 end
