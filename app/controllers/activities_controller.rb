2e4cc7703a7f7f7c14258a9731f3411c975ad033
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-04-24 01:37:23 -0300

    Adjust queries for Heroku

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -65,7 +65,7 @@ class ActivitiesController < ApplicationController
   private
 
   def activities_created
-    Activity.ascending.distinct.order('name ASC').pluck(:name)
+    Activity.distinct.ascending.pluck(:name)
   end
 
   def activity_params
