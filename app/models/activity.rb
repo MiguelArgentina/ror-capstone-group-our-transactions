d4af4b22c072a13dabb4df5125a010cfde000084
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-04-26 14:53:45 -0300

    Update active record association to reflex many-to-many from activities to groups

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,7 +1,7 @@
 class Activity < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :group, optional: true
-  has_many :groups
+  has_and_belongs_to_many :groups
   default_scope -> { order(created_at: :desc) }
   scope :ascending, -> { order(name: :asc) }
 
